58edb0be7a110a4fceb8daf9640d55901d8983bf
Vinicius Correia Souza <154944076+CorreiaSouza@users.noreply.github.com>
2024-02-29 00:07:10 -0300

    2902

--- chat-botarq/chatbot-codigofonte.py
+++ chat-botarq/chatbot-codigofonte.py
@@ -7,6 +7,8 @@ def processar_resposta(pergunta, nome):
         print(f'{os.linesep}{nome}, para se tornar um cientista de dados, você precisa estudar alguns temas que são muito relevantes para a profissão.\nÉ preciso se dedicar e estar sempre em busca de novos conhecimentos.\nUma forma de aprimorar seus conhecimentos é aprender muita coisa legal e se inscrever no Canal NERD DOS DADOS,\nporque é um canal que traz conteúdos atualizados toda semana.\nEntão se inscreva aqui no canal e ative as notificações para sempre ficar por dentro das novidades.{os.linesep}')
     elif "aprender" in pergunta:
         print(f'{os.linesep}{nome}, vale muito a pena aprender Python, pois é uma das linguagens que mais cresce')
+    elif "IA" in pergunta or "Inteligencia Artificial" in pergunta:
+        print(f'{os.linesep}{nome}, IA, ou Inteligência Artificial, refere-se à capacidade de um sistema computacional imitar e executar tarefas que \nnormalmente exigiriam inteligência humana. Isso inclui aprendizado, raciocínio, resolução de problemas, \ncompreensão de linguagem natural, reconhecimento de padrões e tomada de decisões. \nAs IA podem ser categorizadas em várias formas, incluindo IA fraca (específica para uma tarefa ou domínio) e IA forte \n(capaz de realizar uma ampla gama de tarefas com autonomia.{os.linesep}')
     else:
         print(f'{os.linesep}{nome}, desculpe, não entendi sua pergunta.')
 
